014b0c07bcd4a3f579eea0728337c74703f69541
ronenbendavid <46065546+ronenbendavid@users.noreply.github.com>
2021-09-01 22:32:23 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from sklearn.metrics import precision_score, recall_score
 from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.preprocessing import StandardScaler
 
 def main():
     st.title("RIP AI for Auto settings ")    
