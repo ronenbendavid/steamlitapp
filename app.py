036ceceea54a0464d81715b18b051726a0603b9b
ronenbendavid <46065546+ronenbendavid@users.noreply.github.com>
2021-09-02 16:33:19 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import category_encoders as ce
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
