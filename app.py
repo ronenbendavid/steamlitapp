4cdf9ff6b34a8c1a9af1139a4a1a4cf22920ba5b
ronenbendavid <46065546+ronenbendavid@users.noreply.github.com>
2021-09-02 18:41:18 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -202,7 +202,9 @@ def main():
             st.write(cx_test)
             encoded_model = ce.leave_one_out.LeaveOneOutEncoder().fit(x_train,y_train)
             ex_test = encoded_model.transform(cx_test)
-            st.write(cx_test)
+            ex_test = StandardScaler().transform(ex_test)
+            st.write('printing encodex X')
+            st.write(ex_test)
             y_predict = model.predict(ex_test)
             st.write(f'PDF file {pdffilename} optimization: {y_predict}')
     if st.sidebar.checkbox("Show raw data", False):
