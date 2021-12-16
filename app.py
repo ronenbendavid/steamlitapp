43ab982346996d9cd8bf636b8c6470f3a5dba240
ronenbendavid <46065546+ronenbendavid@users.noreply.github.com>
2021-12-16 07:54:24 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -87,9 +87,9 @@ def main():
             #st.write(ex_test)
             y_predict = model.predict(ex_test)
             st.write(f'Optimization Results for file: {pdffilename.name} Type {pdffilename.type} Size {pdffilename.size} is: {y_predict}')
-            line = [info.Creator, info.Producer, str(num_pages), product, 'PDF',str(y_predict)]
+            line = [info.Creator, info.Producer, str(num_pages), product, 'PDF',str(y_predict.reshape((1,1)))]
             history.append(line)
-            st.write(history)
+            st.write(pd.DataFrame(history,columns = ['creator', 'producer', 'pages', 'product', 'type','label']))
     def extarct_pdf_info(pdffilename):
         pdf = PdfReader(pdffilename)
         info = pdf.Info
