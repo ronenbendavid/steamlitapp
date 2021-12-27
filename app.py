511256cccdb4461281eff2929f5daa8f01795111
ronenbendavid <46065546+ronenbendavid@users.noreply.github.com>
2021-12-27 12:58:29 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 import category_encoders as ce
 import os
+import base64
+import json
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
